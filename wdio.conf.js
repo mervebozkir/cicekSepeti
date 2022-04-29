23c03f981d4340f21840fcc686b3045c120259b8
mervebozkir <70012879+mervebozkir@users.noreply.github.com>
2022-04-30 00:36:33 +0300

    login7

--- wdio.conf.js
+++ wdio.conf.js
@@ -159,7 +159,7 @@ exports.config = {
         // <boolean> fail if there are any undefined or pending steps
         strict: false,
         // <string> (expression) only execute the features or scenarios with tags matching the expression
-        tagExpression: '',
+        tagExpression: '@login7',
         // <number> timeout for step definitions
         timeout: 60000,
         // <boolean> Enable this config to treat undefined definitions as warnings.
